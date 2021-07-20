0cc9e370e0e48263e995e3e7b3e6254af10c397c
perecanals <perecanalscanals@gmail.com>
2021-07-20 16:55:33 +0200

    Fixed outputPath

--- arterial/segmentation/nnunetSegmentation.py
+++ arterial/segmentation/nnunetSegmentation.py
@@ -67,7 +67,9 @@ def inference(casePath):
     if not os.path.isdir(outputPath): os.mkdir(outputPath)
     
     # The casePath will be placed in a newly created dir with the patId as name
+    print(os.path.isfile(os.path.join(inputPath, patId + "_0000.nii.gz")))
     if not os.path.isfile(os.path.join(inputPath, patId + "_0000.nii.gz")):
+        print(os.path.join(inputPath, patId + "_0000.nii.gz"))
         os.rename(casePath, os.path.join(inputPath, patId + "_0000.nii.gz"))
 
     # Only use if running on Colab
