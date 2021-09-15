452f74ec01c43abd80f3e01f9da97349173a8431
perecanals <perecanalscanals@gmail.com>
2021-09-15 09:44:33 +0200

    Removed unused setup files

--- arterial/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# Setting up environment variables
-echo "export arterialDir=`pwd`" >> ~/.zshrc
-echo "export nnUNet_raw_data_base=`pwd`/segmentation/nnUNet_base/nnUNet_raw_data" >> ~/.zshrc
-echo "export nnUNet_preprocessed=`pwd`/segmentation/nnUNet_base/nnUNet_preprocessed" >> ~/.zshrc
-echo "export RESULTS_FOLDER=`pwd`/segmentation/models" >> ~/.zshrc
-
-source ~/.zshrc
