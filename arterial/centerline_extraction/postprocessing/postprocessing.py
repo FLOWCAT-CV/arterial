1b0e96de23c5ee17717f4f244b64d2f243f41814
perecanals <perecanalscanals@gmail.com>
2023-03-06 11:45:22 +0100

    Added filter for centerline segments array

--- arterial/centerline_extraction/postprocessing/postprocessing.py
+++ arterial/centerline_extraction/postprocessing/postprocessing.py
@@ -169,11 +169,11 @@ def compute_centerline_segments_array(case_dir):
                     remove_point_idx.append(point_idx)
 
             if len(remove_point_idx) > 0:
-                print(centerline_segments_array[idx, 0])
+                print(centerline_segments_array[idx, 0].dtype, centerline_segments_array[idx, 0].shape)
             centerline_segments_array[idx, 0] = np.delete(centerline_segments_array[idx, 0], remove_point_idx, axis = 0)
             centerline_segments_array[idx, 1] = np.delete(centerline_segments_array[idx, 1], remove_point_idx, axis = 0)
             if len(remove_point_idx) > 0:
-                print(centerline_segments_array[idx, 0])
+                print(centerline_segments_array[idx, 0].dtype, centerline_segments_array[idx, 0].shape)
 
         remove_floating = []
         for idx, segment in enumerate(centerline_segments_array):
