d7ece95e02b3b0c4a68ad6040578ff1bffbcd159
perecanals <perecanalscanals@gmail.com>
2023-11-06 12:11:39 +0100

    Bug fix in single segments plot

--- arterial/feature_extraction/segment_features/utils.py
+++ arterial/feature_extraction/segment_features/utils.py
@@ -1151,7 +1151,7 @@ def plot_single_segments(case_dir, centerline_graph, segments_vessel_type):
         # In order to place the nodes in the visualization of the graph in a sagittal view, we use L and S coordinates (the view will be from the coronal plane, P axis)
         node_pos_dict_P = {}
         for n in segments_vessel_type[vessel_type].nodes():
-            node_pos_dict_P[n] = [segments_vessel_type[vessel_type].nodes(data=True)[n]["pos"][0], segments_vessel_type[vessel_type].nodes(data=True)[n]["pos"][2]]
+            node_pos_dict_P[n] = [-segments_vessel_type[vessel_type].nodes(data=True)[n]["pos"][0], segments_vessel_type[vessel_type].nodes(data=True)[n]["pos"][2]]
         for _ in range(1):
             nx.draw(segments_vessel_type[vessel_type], node_pos_dict_P, node_size = 10, ax = ax[idx // columns, idx % columns])
             ax[idx // columns, idx % columns].set_title(vessel_type, fontsize=12)
