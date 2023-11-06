2bc9f11c83810ff6a5e39c2407ed1605ac4937d9
perecanals <perecanalscanals@gmail.com>
2023-11-06 14:02:38 +0100

    Small correction for supersegment pot

--- arterial/feature_extraction/mapping/utils.py
+++ arterial/feature_extraction/mapping/utils.py
@@ -824,12 +824,12 @@ def select_configuration(case_dir, centerline_graph):
         # In order to place the nodes in the visualization of the graph in a sagittal view, we use L and S coordinates (the view will be from the coronal plane, P axis)
         node_pos_dict_P = {}
         for n in supersegment.nodes():
-            node_pos_dict_P[n] = [supersegment.nodes(data=True)[n]["pos"][0], supersegment.nodes(data=True)[n]["pos"][2]]
+            node_pos_dict_P[n] = [-supersegment.nodes(data=True)[n]["pos"][0], supersegment.nodes(data=True)[n]["pos"][2]]
 
         nx.draw(supersegment, node_pos_dict_P, node_size=10, node_color=colorMap)
         ax.set_title(patient_configuration["Access"] + " + " + patient_configuration["Laterality"] + " + " + patient_configuration["Antero-posterior"] + ". time: " + str(patient_configuration["Time first angiography"]), fontsize=12)
-        ax.set_xlim([8, 200])
-        ax.set_ylim([-10, 280])
+        ax.set_xlim([-200, 10])
+        ax.set_ylim([-10, 350])
             
         plt.savefig(os.path.join(case_dir, "thrombectomy_configuration", "supersegment.png"))
 
