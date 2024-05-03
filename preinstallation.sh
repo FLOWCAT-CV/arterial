f4e05012e19e337ca739e696d2be586b3ab60b2e
perecanals <perecanalscanals@gmail.com>
2024-05-03 13:55:04 +0200

    Small fixes in centerline extraction preprocessing

---
+++ preinstallation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the directory path of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Export environmental variables based on the directory path
+export arterial_dir="$DIR/arterial"
+
+# Install necessary packages before arterial installation
+conda install -c conda-forge vmtk
+pip install --upgrade pip
+pip install torch
+pip install torch_geometric pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.3.0+cu121.html
